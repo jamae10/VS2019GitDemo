17fcdb8c1af1d55241ec000aa7242ea0438ca69b
jamae10 <52740452+jamae10@users.noreply.github.com>
2020-09-09 14:27:52 +0800

    commit to change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace VS2019GitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Jemimah");
         }
     }
 }
+ 
