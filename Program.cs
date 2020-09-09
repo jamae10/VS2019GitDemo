d54e89b3a0765d31cbbfc5fb9b1b53449d2c8b28
jamae10 <52740452+jamae10@users.noreply.github.com>
2020-09-09 14:16:50 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace VS2019GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
